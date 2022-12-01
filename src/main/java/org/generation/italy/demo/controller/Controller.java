3c0bf410de266a8ca924fba63e3111008acac7ce
Luca Spennato <luca.spennato@gmail.com>
2022-12-01 16:09:18 +0100

    bonus

--- src/main/java/org/generation/italy/demo/controller/Controller.java
+++ src/main/java/org/generation/italy/demo/controller/Controller.java
@@ -1,8 +1,8 @@
 package org.generation.italy.demo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.model.Model;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -10,7 +10,9 @@ public class Controller {
 	@RequestMapping("/test")
 	public String getHome(Model model) {
 		
+		String name = "Luco";
 		
+		model.addAttribute("name", name);
 		
 		return "index";
 		
